7b586eb8bae93cf4549444081eddd34b1d007372
AlexandraH-code <ro24nz8c@students.codeinstitute.net>
2025-02-04 08:16:24 +0100

    Add template

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ class PostList(generic.ListView):
     #queryset = Post.objects.all()
     #queryset = Post.objects.filter(author=2)
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
 
     #model = Post
 #def my_blog(request):
